82b4d7fbcef102df34e3f4cceebe841e85ee2c95
CookieOups <amandine.clerc0@gmail.com>
2014-05-24 23:46:00 +0200

    ajout index
    J'avais oublié de mettre la page index

---
+++ index.php
@@ -0,0 +1,494 @@
+<?php
+$bodyClass = "homepage";
+include_once 'inc/header.php';
+?>
+			
+			<div class="container">
+
+				<div class="mosaique">
+
+					<div class="one-beaute mes__blocs">
+						<a href="trend-beaute-cire.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Epilation</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+					<div class="one-nail mes__blocs">
+						<a href="trend-nail-galaxie.php" class="ec-circle circle">
+						<h2 class="time">45min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#EffetGalaxie</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="two-nail mes__blocs">
+						<a href="trend-nail-coeurs.php" class="ec-circle circle">
+							<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#StValentin</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<a href="concours.php" >
+						<div class="mosaique-concours mes__blocs">
+						<p>N'oublie pas notre concours!</p>
+						<div class="go-concours"></div>	
+						</div>
+					</a>
+
+
+					<div class="two-beaute mes__blocs">
+						<a href="trend-beaute-masque.php" class="ec-circle circle">
+						<h2 class="time">5min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Masques</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="one-coiffure mes__blocs">
+						<a href="trend-coiffure-tresses-epis.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Episdeblé</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="three-nail mes__blocs">
+						<a href="trend-nail-liner.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#NailartLiner</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="one-mode mes__blocs">
+						<a href="trend-mode-robechemise.php" class="ec-circle circle">
+						<h2 class="time">15min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#RobeChemise</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="two-coiffure mes__blocs">
+						<a href="trend-coiffure-chignon.php" class="ec-circle circle">
+							<h2 class="time">15min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Donut</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="three-beaute mes__blocs">
+						<a href="trend-beaute-acne.php" class="ec-circle circle">
+						<h2 class="time">5min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Acné</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="two-mode mes__blocs">
+						<a href="trend-mode-pantalontaillehaute.php" class="ec-circle circle">
+						<!-- <h2 class="time">10min</h2> -->
+						<div class="content-circle">
+						    <h2 class="hashtag">#TailleHaute</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<a href="inscription.php">
+						<div class="mosaique-concours mes__blocs">
+						<p>Inscris toi pour proposer tes astuces !</p>
+						<div class="go-concours"></div>
+						</div>
+					</a>
+
+
+					<div class="three-mode mes__blocs">
+						<a href="trend-mode-affinersilhouette.php" class="ec-circle circle">
+						<!-- <h2 class="time">10min</h2> -->
+						<div class="content-circle">
+						    <h2 class="hashtag">#Affiner#Silhouette</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+
+					<div class="four-nail mes__blocs">
+						<a href="trend-nail-striping.php" class="ec-circle circle">
+						<h2 class="time">30min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#StrippingTape</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="four-mode mes__blocs">
+						<a href="trend-mode-couleurs.php" class="ec-circle circle">
+						<!-- <h2 class="time">30min</h2> -->
+						<div class="content-circle">
+						    <h2 class="hashtag">#Couleurs</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="three-coiffure mes__blocs">
+						<a href="trend-coiffure-gras.php" class="ec-circle circle">
+						<h2 class="time">5min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Gras</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="five-mode mes__blocs">
+						<a href="trend-mode-rondeurs.php" class="ec-circle circle">
+						<!-- <h2 class="time">40min</h2> -->
+						<div class="content-circle">
+						    <h2 class="hashtag">#Rondeurs</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="five-nail mes__blocs">
+						<a href="trend-nail-pinup.php" class="ec-circle circle">
+						<h2 class="time">40min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#EffetPinUp</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="four-beaute mes__blocs">
+						<a href="trend-beaute-demaquillant.php" class="ec-circle circle">
+						<h2 class="time">5min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Démaquillant</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="four-coiffure mes__blocs">
+						<a href="trend-coiffure-blondir.php" class="ec-circle circle">
+						<h2 class="time">20min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Blonde</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+
+					<a href="concours.php">
+						<div class="mosaique-concours mes__blocs">
+						<p>Hé! On a un super concours !</p>
+						<div class="go-concours"></div>
+						</div>
+					</a>
+
+					<div class="six-nail mes__blocs">
+						<a href="trend-nail-ombrenails.php" class="ec-circle circle">
+						<h2 class="time">45min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#OmbreNails</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="five-beaute mes__blocs">
+						<a href="trend-beaute-cafe.php" class="ec-circle circle">
+						<h2 class="time">5min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Café</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="six-beaute mes__blocs">
+						<a href="trend-beaute-crayonblanc.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#CrayonBlanc</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<a href="inscription.php">
+						<div class="mosaique-concours mes__blocs">
+						<p>Tu as des astuces sympa ? Inscris-toi!</p>
+						<div class="go-concours"></div>
+						</div> 
+					</a>
+
+
+					<div class="six-mode mes__blocs">
+						<a href="trend-mode-short.php" class="ec-circle circle">
+						<h2 class="time">45min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#ShortRomantique</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="seven-mode mes__blocs">
+						<a href="trend-mode-perfecto.php" class="ec-circle circle">
+						<!-- <h2 class="time">20min</h2> -->
+						<div class="content-circle">
+						    <h2 class="hashtag">#Perfecto</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<a href="concours.php">
+						<div class="mosaique-concours mes__blocs">
+						<p>Participe à notre concours de cette semaine !</p>
+						<div class="go-concours"></div>
+						</div>
+					</a>
+
+					<div class="seven-beaute mes__blocs">
+						<a href="trend-beaute-jour.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Jour</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="seven-nail mes__blocs">
+						<a href="trend-nail-dotting.php" class="ec-circle circle">
+						<h2 class="time">20min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#DottingTool</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="eight-beaute mes__blocs">
+						<a href="trend-beaute-mascara.php" class="ec-circle circle">
+						<h2 class="time">10min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Mascara</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="eight-nail mes__blocs">
+						<a href="trend-nail-stamping.php" class="ec-circle circle">
+						<h2 class="time">20min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#Stamping</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="eight-mode mes__blocs">
+						<a href="trend-mode-bracelet.php" class="ec-circle circle">
+						<h2 class="time">45min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#BraceletPerle</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="nine-mode mes__blocs">
+						<a href="trend-mode-tshirt.php" class="ec-circle circle">
+						<h2 class="time">30min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#CutShirt</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+					<div class="nine-beaute mes__blocs">
+						<a href="trend-beaute-cateye.php" class="ec-circle circle">
+						<h2 class="time">20min</h2>
+						<div class="content-circle">
+						    <h2 class="hashtag">#CatEye</h2>
+		           			<div class="go"></div>
+						</div>
+						</a>
+					</div>
+
+				</div>
+
+			</div><!-- end container -->
+
+			<footer>
+
+				<p class="copyright">&copy; Fait avec amour par Amandine Clerc</a></p>
+
+				<div id="social" class="social">
+						<a class="instagram" href="http://instagram.com/trendy_mode0/" target="_blank">Instagram</a>
+						<a class="twitter" href="https://twitter.com/Trendy__mode" target="_blank">twitter</a>
+						<a class="facebook" href="https://www.facebook.com/trendymode0" target="_blank">facebook</a>
+				</div>
+
+				<p><a class="footer-about" href="about.php">A propos de Trendy</a></p>
+
+			</footer>
+			
+		</div><!-- end content -->
+
+
+
+
+	<script>
+		var pageWidth = document.body.clientWidth,
+				blocs = document.getElementsByClassName("mes__blocs"),
+				blocParColonne = 5;
+				console.log(document.documentElement.clientWidth);
+				console.log(document.documentElement.scrollWidth);
+		Array.prototype.forEach.call(blocs, function(el, i){
+
+			console.log("azertyu")
+
+			var size = (pageWidth/blocParColonne).toFixed(2);
+			blocs[i].style.width = size +"px";
+			blocs[i].style.height = pageWidth/blocParColonne+"px";
+		});
+
+		var navBut = document.getElementById("navBut");
+		var header = document.getElementById("header");
+		var menu = document.getElementById("menu");
+		var logo = document.getElementsByClassName("logo")[0];
+		var content = document.getElementsByClassName("content")[0];
+		var social = document.getElementsByClassName("social")[0];
+		var show = true;
+
+		function showMenu(){
+
+			if(show == true){
+
+				show = false;
+
+				header.className = "";
+				menu.className = "";
+				social.className = "";
+				logo.classList.remove("hide");
+				content.classList.add("move");
+				social.classList.remove("hide");
+			}
+
+			else{
+
+				show = true;
+
+				header.className = "hide";
+				menu.className = "hide";
+				social.className = "hide";
+				logo.classList.add("hide");
+				social.classList.add("hide");
+				content.classList.remove("move");
+
+			}
+
+		};
+
+		function showMenu(){
+
+			if(show == true){
+
+				show = false;
+
+				header.className = "";
+				menu.className = "";
+				social.className = "";
+				logo.classList.remove("hide");
+				content.classList.add("onmousehover");
+				social.classList.remove("hide");
+			}
+
+			else{
+
+				show = true;
+
+				header.className = "hide";
+				menu.className = "hide";
+				social.className = "hide";
+				logo.classList.add("hide");
+				social.classList.add("hide");
+				content.classList.remove("onmousehover");
+
+			}
+
+		};
+
+		header.addEventListener("mouseover",showMenu,false);
+		header.addEventListener("mouseout",showMenu,true);
+
+		header.addEventListener("click",showMenu,false);
+
+
+
+		//http://codepen.io/Metty/pen/dglwH CREDITER 
+		// Dropdown Menu
+		var dropdown = document.querySelectorAll('.dropdown');
+		var dropdownArray = Array.prototype.slice.call(dropdown,0);
+		dropdownArray.forEach(function(el){
+			var button = el.querySelector('a[data-toggle="dropdown"]'),
+					menu = el.querySelector('.dropdown-menu'),
+					arrow = button.querySelector('i.icon-arrow');
+
+			button.onclick = function(event) {
+				if(!menu.hasClass('show')) {
+					menu.classList.add('show');
+					menu.classList.remove('hide');
+					arrow.classList.add('open');
+					arrow.classList.remove('close');
+					event.preventDefault();
+				}
+				else {
+					menu.classList.remove('show');
+					menu.classList.add('hide');
+					arrow.classList.remove('open');
+					arrow.classList.add('close');
+					event.preventDefault();
+				}
+			};
+		})
+
+		Element.prototype.hasClass = function(className) {
+		    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+		};
+
+
+	</script>
+</body>
+</html>
